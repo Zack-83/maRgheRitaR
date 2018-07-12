307dfdcc42a8adf6edf0718ab8a2efeefc64c152
Zack-83 <giacomo.lanza@ptb.de>
2018-07-12 14:01:00 +0200

    First commitment

---
+++ R/center.R
@@ -0,0 +1,17 @@
+#' Centering datasets
+#'
+#' Some more description
+#'
+#' And even more
+#'
+#' @param dataset The numeric vector to be centered
+#' @param desired The numeric midpoint value around which the data will be centered
+#'
+#' @return A new vector containing the original data centered around the desired values
+#'
+#' @examples
+#	center(c(1,2,3),0) # should return -1 0 1
+#	center(c(4,5,6),2) # should return 1 2 3
+center <- function(dataset, desired) {
+	dataset - mean(dataset) + desired
+}
